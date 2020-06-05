cac9eb1b3cd74f4cb3dcd26ca220d77613b7b039
Wes <wesleySalesberry@hotmail.com>
2020-06-05 10:20:13 -0400

    inital commit

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,58 +1,81 @@
-import React, { useState } from "react";
-import ProptTypes from "prop-types";
-import { Loader } from "../loader";
-import "./LoginForm.css";
+import React, { useState } from 'react';
+import ProptTypes from 'prop-types';
+import { Loader } from '../loader';
+import './LoginForm.css';
+
+import { Link } from 'react-router-dom';
+
+import { Grid, Form, Segment, Header, Message, Icon } from 'semantic-ui-react';
 
 export const LoginForm = ({ login, loading, error }) => {
-  // Not to be confused with "this.setState" in classes
-  const [state, setState] = useState({
-    username: "",
-    password: "",
-  });
+	// Not to be confused with "this.setState" in classes
+	const [ state, setState ] = useState({
+		username: '',
+		password: ''
+	});
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    login(state);
-  };
+	const isFormValid = ({ username, passwords }) => username && passwords;
 
-  const handleChange = (event) => {
-    const inputName = event.target.name;
-    const inputValue = event.target.value;
-    setState((prevState) => ({ ...prevState, [inputName]: inputValue }));
-  };
+	const handleLogin = (event) => {
+		event.preventDefault();
+		login(state);
+	};
 
-  return (
-    <React.Fragment>
-      <form id="login-form" onSubmit={handleLogin}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={state.username}
-          autoFocus
-          required
-          onChange={handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={state.password}
-          required
-          onChange={handleChange}
-        />
-        <button type="submit" disabled={loading}>
-          Login
-        </button>
-      </form>
-      {loading && <Loader />}
-      {error && <p style={{ color: "red" }}>{error.message}</p>}
-    </React.Fragment>
-  );
+	const handleChange = (event) => {
+		const inputName = event.target.name;
+		const inputValue = event.target.value;
+		setState((prevState) => ({ ...prevState, [inputName]: inputValue }));
+	};
+
+	return (
+		<Grid textAlign="center" verticalAlign="middle" className="app">
+			<Grid.Column style={{ maxWidth: 450 }}>
+				<Header as="h1" icon color="black" textAlign="center">
+					<Icon name="globe" color="black" />
+					Register for devChat!
+				</Header>
+				<Form id="login-form" onSubmit={handleLogin} size="large">
+					<Segment stacked>
+						<Form.Input
+							autoFocus
+							required
+							fluid
+							name="username"
+							icon="user"
+							iconPosition="left"
+							placeholder="Username"
+							type="text"
+							value={state.username}
+							onChange={handleChange}
+						/>
+						<Form.Input
+							fluid
+							name="password"
+							icon="lock"
+							iconPosition="left"
+							placeholder="Password"
+							type="password"
+							value={state.password}
+							onChange={handleChange}
+						/>
+						<div type="submit" disabled={loading} className={loading ? 'ui buttons loading' : 'ui buttons'}>
+							<button className="ui button">Submit</button>
+							<div className="or" />
+							<button className="ui primary button">Google Login</button>
+						</div>
+					</Segment>
+				</Form>
+				{error && <Message style={{ color: 'red' }}>{error.message}</Message>}
+				<Message>
+					Need to Sign Up? <Link to="/register"> Sign Up</Link>
+				</Message>
+			</Grid.Column>
+		</Grid>
+	);
 };
 
 LoginForm.propTypes = {
-  login: ProptTypes.func.isRequired,
-  loading: ProptTypes.bool,
-  error: ProptTypes.string,
+	login: ProptTypes.func.isRequired,
+	loading: ProptTypes.bool,
+	error: ProptTypes.string
 };
