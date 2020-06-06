28b69732e2e23b8d8217e98ebc4b877858221701
Wes <wesleySalesberry@hotmail.com>
2020-06-05 20:42:24 -0400

    added sign up route

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,24 +1,16 @@
-import React from "react";
-import { BrowserRouter, Switch } from "react-router-dom";
-import { HomeScreen, ProfileScreen, NotFoundScreen } from "../../screens";
-import { ConnectedRoute } from "../connected-route/ConnectedRoute";
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { HomeScreen, ProfileScreen, NotFoundScreen } from '../../screens';
+import { ConnectedRoute } from '../connected-route/ConnectedRoute';
+import { Register } from '../register/Register';
 
 export const Navigation = () => (
-  <BrowserRouter>
-    <Switch>
-      <ConnectedRoute
-        exact
-        path="/"
-        redirectIfAuthenticated
-        component={HomeScreen}
-      />
-      <ConnectedRoute
-        exact
-        isProtected
-        path="/profiles/:username"
-        component={ProfileScreen}
-      />
-      <ConnectedRoute path="*" component={NotFoundScreen} />
-    </Switch>
-  </BrowserRouter>
+	<BrowserRouter>
+		<Switch>
+			<ConnectedRoute exact path="/" redirectIfAuthenticated component={HomeScreen} />
+			<ConnectedRoute exact path="/register" redirectIfAuthenticated component={Register} />
+			<ConnectedRoute exact isProtected path="/profiles/:username" component={ProfileScreen} />
+			<ConnectedRoute path="*" component={NotFoundScreen} />
+		</Switch>
+	</BrowserRouter>
 );
