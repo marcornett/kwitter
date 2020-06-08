7a1e9a6af355640ee38bc83d3a6a44fcc6041bf1
Wes <wesleySalesberry@hotmail.com>
2020-06-08 12:03:18 -0400

    added the route to the create account

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import { HomeScreen, ProfileScreen, NotFoundScreen } from '../../screens';
+import { HomeScreen, ProfileScreen, NotFoundScreen, RegisterScreen } from '../../screens';
 import { ConnectedRoute } from '../connected-route/ConnectedRoute';
-import { Register } from '../register/Register';
 
 export const Navigation = () => (
 	<BrowserRouter>
 		<Switch>
 			<ConnectedRoute exact path="/" redirectIfAuthenticated component={HomeScreen} />
-			<ConnectedRoute exact path="/register" redirectIfAuthenticated component={Register} />
+			<ConnectedRoute exact path="/register" redirectIfAuthenticated component={RegisterScreen} />
 			<ConnectedRoute exact isProtected path="/profiles/:username" component={ProfileScreen} />
 			<ConnectedRoute path="*" component={NotFoundScreen} />
 		</Switch>
