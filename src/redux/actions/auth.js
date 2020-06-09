0c771c43e91b627b98b1ac726d108bae9ebe9f4a
Wes <wesleySalesberry@hotmail.com>
2020-06-09 09:40:58 -0400

    started the state for register

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -4,8 +4,13 @@ import api from '../../utils/api';
 export const LOGIN = 'AUTH/LOGIN';
 export const LOGIN_SUCCESS = 'AUTH/LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'AUTH/LOGIN_FAILURE';
-export const LOGOUT = 'AUTH/LOGOUT';
 
+//New user request
+export const REGISTER_REQUEST = 'AUTH/USERS_REGISTER_REQUEST';
+export const REGISTER_SUCCESS = 'AUTH/USERS_REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'AUTH/USERS_REGISTER_FAILURE';
+
+export const LOGOUT = 'AUTH/LOGOUT';
 /*
  AUTH ACTIONS (this is a thunk....)
  THUNKS: --> https://github.com/reduxjs/redux-thunk#whats-a-thunk
@@ -26,21 +31,6 @@ export const login = (credentials) => async (dispatch, getState) => {
 	}
 };
 
-export const register = (credentials) => async (dispatch, getstate) => {
-	console.log(credentials.username, credentials.displayName);
-	try {
-		//console.log(credentials);
-		dispatch({ type: LOGIN });
-		const payload = await api.createUser(credentials);
-		dispatch({ type: LOGIN_SUCCESS, payload });
-	} catch (err) {
-		dispatch({
-			type: LOGIN_FAILURE,
-			payload: err.message
-		});
-	}
-};
-
 export const logout = () => async (dispatch, getState) => {
 	try {
 		// We do not care about the result of logging out
@@ -51,6 +41,20 @@ export const logout = () => async (dispatch, getState) => {
      * Let the reducer know that we are logged out
      */
 		dispatch({ type: LOGOUT });
+	}
+};
+
+export const register = (credentials) => async (dispatch, getstate) => {
+	try {
+		//console.log(credentials);
+		dispatch({ type: REGISTER_REQUEST });
+		const payload = await api.createUser(credentials);
+		dispatch({ type: REGISTER_SUCCESS, payload });
+	} catch (err) {
+		dispatch({
+			type: REGISTER_FAILURE,
+			payload: err.message
+		});
 	}
 };
 // END AUTH ACTIONS
