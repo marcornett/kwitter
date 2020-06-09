92ab45ca1a58649256016fe7411e289e255a8cef
Wes <wesleySalesberry@hotmail.com>
2020-06-09 09:42:58 -0400

    removed uneeded code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,7 @@
-import { combineReducers } from "redux";
-import { authReducer } from "./auth";
+import { combineReducers } from 'redux';
+import { authReducer } from './auth';
+import { authRegisterReducer } from './registerReducer';
 
-export default combineReducers({ auth: authReducer });
+export default combineReducers({
+	auth: authReducer
+});
