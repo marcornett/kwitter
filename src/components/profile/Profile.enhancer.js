8d2a94f8643c7db88fddfe9841fe85f0530d31d5
Marcel Cornett <marcornett@gmail.com>
2020-06-09 13:59:19 -0400

    Mapped dispatch and state to props. Added delete user action to remove user data from Kwitter API.

--- src/components/profile/Profile.enhancer.js
+++ src/components/profile/Profile.enhancer.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { getUser } from "../../redux/actions/users";
+import { getUser, deleteUser } from "../../redux/actions/users";
 
 // https://react-redux.js.org/using-react-redux/connect-mapstate#connect-extracting-data-with-mapstatetoprops
 const mapStateToProps = (state) => ({
@@ -9,12 +9,14 @@ const mapStateToProps = (state) => ({
     createdAt: state.user.createdAt,
     updatedAt: state.user.updatedAt,
     pictureLocation: state.user.pictureLocation,
-    googleId: state.user.googleId
+    googleId: state.user.googleId,
+    state: state.auth.isAuthenticated
 });
 
 // https://react-redux.js.org/using-react-redux/connect-mapdispatch#connect-dispatching-actions-with-mapdispatchtoprops
 const mapDispatchToProps = {
     getUser,
+    deleteUser,
 };
 
 export const enhancer = connect(mapStateToProps, mapDispatchToProps);
