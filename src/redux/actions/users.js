9fa15fd784e02360614a8f6308d6332f5f6eed65
Marcel Cornett <marcornett@gmail.com>
2020-06-09 16:22:22 -0400

    Added delete user action to make API call and dispatch DELETE_USER & LOGOUT once the button is clicked in the component.

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -4,13 +4,13 @@ export const DELETE_USER = 'USERS/DELETE_USER'
 export const USER = 'USERS/USER'
 export const PROFILE_PICTURE = 'USERS/PROFILE_PICTURE'
 export const USER_FAILURE = 'USERS/USER_FAILURE'
+export const LOGOUT = 'AUTH/LOGOUT';
 
 
 export const getUser = (username) => async (dispatch, getState) => {
     try {
         const payload = await api.getUserInfo(username);
         // ℹ️ℹ️This is how you would debug the response to a request ℹ️ℹ️
-        console.log({ payload })
         dispatch({
             type: USER,
             payload
@@ -20,5 +20,23 @@ export const getUser = (username) => async (dispatch, getState) => {
             type: USER_FAILURE,
             payload: err.message
         });
+    }
+};
+
+export const deleteUser = (username) => async (dispatch, getState) => {
+    try {
+        await api.deleteUser(username);
+        // ℹ️ℹ️This is how you would debug the response to a request ℹ️ℹ️
+        dispatch({ type: DELETE_USER });
+    } catch (err) {
+        dispatch({
+            type: USER_FAILURE,
+            payload: err.message
+        });
+    } finally {
+		/**
+     * Let the reducer know that we are logged out
+     */
+        dispatch({ type: LOGOUT });
     }
 };
