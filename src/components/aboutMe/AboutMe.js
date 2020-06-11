9a736f3162a45c55c370c982d10dd572d66a7fb3
Wes <wesleySalesberry@hotmail.com>
2020-06-11 15:54:40 -0400

    add state and created the input fields

--- src/components/aboutMe/AboutMe.js
+++ src/components/aboutMe/AboutMe.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import { Grid, Form, Segment, Header } from 'semantic-ui-react';
 
 export const AboutMe = () => {
-	const [ AboutMe, updatAboutSection ] = useState();
+	const [ user, updatAboutSection ] = useState({
+		username: '',
+		password: '',
+		newAboutMe: '',
+		saved: false
+	});
 	const handleChange = (evt) => {
 		const inputName = evt.target.name;
 		const inputValue = evt.target.value;
@@ -10,14 +15,16 @@ export const AboutMe = () => {
 	};
 	const handleSubmit = (evt) => {
 		evt.preventDefault();
+		updatAboutSection((prevState) => ({ ...prevState, saved: true }));
 	};
 	return (
 		<Grid textAlign="center" verticalAlign="middle" className="app">
 			<Grid.Column style={{ maxWidth: 450 }}>
+				{user.saved ? <div>Saved!</div> : null}
 				<Header as="h2" textAlign="center">
 					About Me
 				</Header>
-				<Form class="ui form">
+				<Form onSubmit={handleSubmit} class="ui form">
 					<Segment stacked>
 						<Form.Input
 							required
@@ -29,6 +36,8 @@ export const AboutMe = () => {
 							iconPosition="left"
 							placeholder="Display Name"
 							type="text"
+							value={user.username}
+							onChange={handleChange}
 						/>
 						<Form class="field ui">
 							<textarea
@@ -37,6 +46,20 @@ export const AboutMe = () => {
 								rows="5"
 								placeholder="What do you want to say about yourself"
 								type="text"
+								value={user.newAboutMe}
+								onChange={handleChange}
+							/>
+
+							<Form.Input
+								fluid
+								required
+								name="password"
+								icon="lock"
+								iconPosition="left"
+								placeholder="Password"
+								type="password"
+								value={user.password}
+								onChange={handleChange}
 							/>
 						</Form>
 						<button class="ui fluid button">Update</button>
