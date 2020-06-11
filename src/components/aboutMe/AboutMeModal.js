7cd03f90e5718d3788961456c82678c8089818a3
Wes <wesleySalesberry@hotmail.com>
2020-06-11 15:55:48 -0400

    created a modal component instead of another page

---
+++ src/components/aboutMe/AboutMeModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AboutMe } from './AboutMe';
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
+
+export const AboutMeModal = ({ pictureLocation, displayName }) => {
+	return (
+		<Modal trigger={<Button>Update Your AboutMe</Button>}>
+			<Modal.Header>Update Your Profile</Modal.Header>
+			<Modal.Content image>
+				<Image
+					wrapped
+					size="medium"
+					src="https://3.bp.blogspot.com/-qDc5kIFIhb8/UoJEpGN9DmI/AAAAAAABl1s/BfP6FcBY1R8/s1600/BlueHead.jpg"
+				/>
+				<Modal.Description>
+					<Header>Hello {displayName}</Header>
+					<AboutMe />
+				</Modal.Description>
+			</Modal.Content>
+		</Modal>
+	);
+};
