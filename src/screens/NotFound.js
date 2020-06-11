56a00af0c853d29cc4cdd7825516e07776098657
Timothy Reynoso <domex2467@gmail.com>
2020-06-11 19:46:01 -0400

    restuctored div tags and ids also centered main container

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './NotFound.css'
 
 const NotFound = ({ location }) => (
-  <>
-    <p>Page not found for {location.pathname}</p>
-    <Link to="/">Go Home</Link>
-  </>
+  <React.Fragment >
+    <div id='NotFoundContainer'>
+      <div id='NotFoundMessage'> 
+        <p>Page not found for {location.pathname}</p>
+        <Link to="/">Go Home</Link>
+      </div>
+   </div>
+  </React.Fragment>
 );
 
 export const NotFoundScreen = NotFound;
