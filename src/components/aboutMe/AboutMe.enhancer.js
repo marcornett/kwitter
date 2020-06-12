73e5c6fd5e56793f010a57e24df7c33eeab12c20
Wes <wesleySalesberry@hotmail.com>
2020-06-12 14:56:15 -0400

    created a enhancer

---
+++ src/components/aboutMe/AboutMe.enhancer.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { updateUser } from '../../redux/actions/users';
+
+// https://react-redux.js.org/using-react-redux/connect-mapstate#connect-extracting-data-with-mapstatetoprops
+const mapStateToProps = (state) => ({
+	username: state.user.username,
+	displayName: state.user.displayName,
+	about: state.user.about,
+	createdAt: state.user.createdAt,
+	updatedAt: state.user.updatedAt,
+	pictureLocation: state.user.pictureLocation,
+	googleId: state.user.googleId
+});
+
+// https://react-redux.js.org/using-react-redux/connect-mapdispatch#connect-dispatching-actions-with-mapdispatchtoprops
+const mapDispatchToProps = {
+	updateUser
+};
+
+export const enhancer = connect(mapStateToProps, mapDispatchToProps);
