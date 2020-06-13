0832aa0cafb5727b9224f7549e57ef4eb9fcc57c
Timothy Reynoso <domex2467@gmail.com>
2020-06-13 18:49:52 -0400

    Added handlePostMessage & handleChange functions to feed.js as well as added the message reducer

--- src/redux/reducers/messages.js
+++ src/redux/reducers/messages.js
@@ -1,10 +1,8 @@
 // TODO: implement
 import {GET_MESSAGES, GETMESSAGES_FAILED, POSTMESSAGE, POSTMESSAGE_SUCCESS} from "../actions"
-
 const initialState = {
   messages:[]
 };
-
 export const messagesReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_MESSAGES:
@@ -13,10 +11,14 @@ export const messagesReducer = (state = initialState, action) => {
       messages: action.payload
     }
     case POSTMESSAGE:
-      return state
+      return {
+        ...state,
+        messages: [...state.messages, action.payload]
+      }
     case POSTMESSAGE_SUCCESS:
       return state
     default:
       return state;
   }
 };
+//current messages
